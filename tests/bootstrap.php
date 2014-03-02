72b6058b7d6be7dd438472a6fe5a93312ae43e26
Lars Moelleken <lars@moelleken.org>
2014-03-03 00:42:53 +0100

    + added: phpunit.xml && bootstrap for "travis-ci" v2

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,9 @@
 <?php
 
-require_once dirname(__DIR__) . '/vendor/composer/autoload.php';
+if (is_file(dirname(__DIR__) . '/URLify.php')) {
+  # for netbeans
+  require_once dirname(__DIR__) . '/URLify.php';  
+} else {
+  # for travis-ci
+  require_once dirname(__DIR__) . '/vendor/composer/autoload.php';
+}
