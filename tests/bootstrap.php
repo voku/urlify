e5d2ea06beba1702956eb8cd2609fa6ca1a0e556
Lars Moelleken <lmoelleken@menadwork.com>
2015-01-28 01:30:59 +0100

    [+]: try to fix "PHPUnit"-Tests for "Travis-CI"

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,3 @@
 <?php
 
-if (is_file(dirname(__DIR__) . '/URLify.php')) {
-  # for netbeans
-  require_once dirname(__DIR__) . '/URLify.php';  
-} else {
-  # for travis-ci
-  require_once dirname(__DIR__) . '/vendor/composer/autoload.php';
-}
+require_once dirname(__DIR__) . '/vendor/composer/autoload.php';
